89c8e52cb908db06aae946c49d8906edd69ef139
humanjuan <jalejandro.ingeniero@gmail.com>
2025-12-09 19:58:07 -0300

    feat: improve type handling in data formatting method
    - Added support for common numeric and boolean types using `fmt.Sprint` for flexible formatting.
    - Enhanced string formatting logic with conditional `fmt.Sprintf` handling for custom patterns.

--- acacia.go
+++ acacia.go
@@ -1023,10 +1023,17 @@ func (_log *Log) formatMessageString(data interface{}, args ...interface{}) stri
 			return v
 		case []byte:
 			return string(v)
+		case int, int8, int16, int32, int64, float32, float64, bool:
+			return fmt.Sprint(v)
 		default:
 			return fmt.Sprint(v)
 		}
 	}
+
+	if fmtStr, ok := data.(string); ok {
+		return fmt.Sprintf(fmtStr, args...)
+	}
+
 	return fmt.Sprintf(data.(string), args...)
 }
 
